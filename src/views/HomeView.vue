1b0da581b66d5ea4563fa8d0d6c875793f5a2826
Chai Lyden <chai.lyden@optistech.com>
2023-05-23 16:29:56 +0700

    fixing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,55 +1,65 @@
 <template>
   <div class="w-full h-auto">
     <div
-      class="container flex justify-between my-4 h-[500px] mb-4 border-collapse"
+      class="container flex justify-between my-4 w-full h-[500px] mb-4 border-collapse"
     >
-      <div class="features_post flex flex-col h-full w-[73.5%]">
+      <div class="features_post flex flex-col h-full w-[73.3%]">
         <div class="features_post_top w-full h-[500px] flex">
           <div
-            class="w-[485px] h-[250px] relative"
+            class="w-[60%] h-[250px] relative"
             style="
               background-image: url(https://cdn.sabay.com/cdn/media.sabay.com/media/Chin-Sopheak/CS-17/646c2c2b289b5_1684810740_large.jpg);
-              background-size: 486px 250px;
+              background-size: 100% 250px;
               background-position: center;
               background-repeat: no-repeat;
             "
           >
-            <div class="absolute">
+            <!-- <div class="absolute">
               <div class="bg-black w-[100px]">Click me</div>
             </div>
+            <div class="absolute bottom-0">
+              <div class="bg-green-400 w-[100px]">Click me</div>
+            </div> -->
           </div>
           <div
-            class="w-[346px] h-[250px]"
+            class="w-[40%] h-[250px] relative"
             style="
               background-image: url(https://cdn.sabay.com/cdn/media.sabay.com/media/Simala/May-PR-Article/646c5d4489905_1684823340_large.jpg);
-              background-size: 346px 250px;
+              background-size: 100% 250px;
               background-position: center;
               background-repeat: no-repeat;
             "
           ></div>
         </div>
-        <div class="features_post_bottom w-full h-[500px] flex">
+        <div class="features_post_top w-full h-[500px] flex">
           <div
-            class="w-[485px] h-[250px]"
+            class="w-[60%] h-[250px] relative"
             style="
               background-image: url(https://cdn.sabay.com/cdn/media.sabay.com/media/Chin-Sopheak/CS-17/646c2c2b289b5_1684810740_large.jpg);
-              background-size: 486px 250px;
+              background-size: 100% 250px;
               background-position: center;
               background-repeat: no-repeat;
             "
-          ></div>
+          >
+            <!-- <div class="absolute">
+              <div class="bg-black w-[100px]">Click me</div>
+            </div>
+            <div class="absolute bottom-0">
+              <div class="bg-green-400 w-[100px]">Click me</div>
+            </div> -->
+          </div>
           <div
-            class="w-[346px] h-[250px]"
+            class="w-[40%] h-[250px] relative"
             style="
               background-image: url(https://cdn.sabay.com/cdn/media.sabay.com/media/Simala/May-PR-Article/646c5d4489905_1684823340_large.jpg);
-              background-size: 346px 250px;
+              background-size: 100% 250px;
               background-position: center;
               background-repeat: no-repeat;
             "
           ></div>
         </div>
       </div>
-      <div class="top-right-ads w-[26.5%]">
+      <div class="top-right-ads w-[26%]">
         <img
           src="https://ads.sabay.com/images/478ff8e39b041fe5d475200ffe81d645.jpg"
           class="w-full h-[248px] mb-1.5"
